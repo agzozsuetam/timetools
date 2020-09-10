4c65663c617ec3f01d8ac877db295ad1ac8be032
agzozsuetam <mateusz.ozga@interia.pl>
2020-09-10 23:53:40 +0200

    Update #1

--- stoper/js/stopwatch.js
+++ stoper/js/stopwatch.js
@@ -5,19 +5,27 @@ var h = 0;
 var m = 0;
 var s = 0;
 var ms = 0;
+var interval = null;
 //var al = new Audio("Alarma.mp3");
 //var check = 0;
 
 st.onclick = function(){
-    window.setInterval(timeLoop, 10);
-}
+    if(st.innerHTML=="START"){
+        interval = window.setInterval(timeLoop, 10);
+        st.innerHTML = "STOP";
+    }else{
+        window.clearInterval(interval);
+        st.innerHTML = "START";
+    };
+};
 
 res.onclick = function(){
     h=0;
     m=0;
     s=0;
     ms=0;
-}
+    display();
+};
 
 function timeLoop(){
     if(ms<99){
@@ -31,15 +39,15 @@ function timeLoop(){
     }else{
         m = m-60;
         h++;
-    }
+    };
     display();
-}
+};
 
 function display(){
     document.getElementById("hour").innerHTML = h>9 ? h : "0"+h;
     document.getElementById("minute").innerHTML = m>9 ? m : "0"+m;
     document.getElementById("second").innerHTML = s>9 ? s : "0"+s;
     document.getElementById("milisecond").innerHTML = ms>9 ? ms : "0"+ms;
-}
+};
 
 //window.setInterval(timeLoop, 1000);
